92be22af794b7b78b5f74ea3099e2869979d6a98
Beaudine den Dunnen <beaudinedendunnen@Beaudines-MacBook.local>
2019-08-26 20:09:52 +0200

    validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ def new
 end
 
 def create
-  current_user.places.create(place_params)
-  redirect_to root_path
+  @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
 end
 
 def show
@@ -32,14 +36,18 @@ def update
     return render plain: 'Not Allowed', status: :forbidden
   end
   @place.update_attributes(place_params)
-  redirect_to root_path
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end 
 end
 
 def destroy
   @place = Place.find(params[:id])
   if @place.user != current_user
     return render plain: 'Not Allowed', status: :forbidden
-  end 
+  end
   @place.destroy
   redirect_to root_path
 end
