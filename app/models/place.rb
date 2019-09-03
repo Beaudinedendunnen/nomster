73759c1447d4f63fc48c349797a738ff15e6153e
Beaudine den Dunnen <beaudinedendunnen@Beaudines-MacBook.local>
2019-09-03 18:48:16 +0200

    colins bugfixing commit.
    here i am in github open source history. the shift key is not
    working correctly on this mac keyboard.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
-  has_many :comments 
+  has_many :comments
   geocoded_by :address
   after_validation :geocode
 
-  validates :name, presence: true, length: {minimum: 2, maximum: 50}
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :address, :description, presence: true
-
 end
