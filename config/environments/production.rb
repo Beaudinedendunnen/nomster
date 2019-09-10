3f2b326451fdd346d81cf073c5bede2b5e272b8a
Beaudine den Dunnen <beaudinedendunnen@Beaudines-MacBook.local>
2019-09-10 14:51:28 +0200

    adding carousel index page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,12 +3,12 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'yourdomain.com',
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
-  } 
+  }
 
   config.action_mailer.default_url_options = {host: 'nomster-beaudine-den-dunnen.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
